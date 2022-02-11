c5d30b4e4cb378f9749efebe118d541227190e82
Marvin Ebuenga <mrx.pandr@gmail.com>
2022-02-12 02:13:20 +0800

    save photos to public folder to use as background

--- components/Countdown/Countdown.js
+++ components/Countdown/Countdown.js
@@ -1,8 +1,12 @@
+import Image from 'next/image'
 import styles from '../../styles/Countdown.module.css'
 
 const Countdown = ({days, hours, minutes, seconds}) => {
   return (
-      <div className={styles.countdown__bg}>
+      <div>
+          <div style={{ position: "fixed", width: "100%", height: "100%", zIndex: -1}} >
+            <Image src="/images/countdown_bg.jpg" alt="countdown" layout="fill" objectFit="cover" />
+          </div>
           <div className={styles.countdown__title__container}>
             <div className={styles.countdown__title}>Comming soon</div>
             <div className={styles.countdown__caption}>{`Happy Valentine's Day`}</div>
